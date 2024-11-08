024521a48bc00a61bfcfe6299162b3755fb82130
mang1024 <wanwangguan@gmail.com>
2024-11-08 11:51:52 +0900

    network3.sh

--- network3.sh
+++ network3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 下载并解压 ubuntu-node-v1.0
-wget https://network3.io/ubuntu-node-v1.0.tar
-tar -xf ubuntu-node-v1.0.tar
+wget https://network3.io/ubuntu-node-v2.1.0.tar
+tar -xf ubuntu-node-v2.1.0.tar
 
 # 切换到目录并运行 manager.sh up
 cd ubuntu-node
@@ -10,8 +10,8 @@ sudo bash manager.sh up
 
 # 返回上级目录
 cd ..
-wget https://network3.io/ubuntu-node-v1.1.tar
-tar -xf ubuntu-node-v1.1.tar
+wget https://network3.io/ubuntu-node-v2.1.0.tar
+tar -xf ubuntu-node-v2.1.0.tar
 
 # 切换到目录并运行 manager.sh key
 cd ubuntu-node
